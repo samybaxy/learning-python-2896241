3636dfb46149ed989827d6c2afd5bd080049eb9c
Samuel Nwaokoro <work@samuelnwaokoro.com>
2023-05-24 13:39:15 +0100

    ch2 data types, indentations, slices and skips

--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -9,29 +9,38 @@ myint = 5
 myfloat = 13.2
 mystr = "This is a string"
 mybool = True
-mylist = [0, 1, "two", 3.2, False]
+mylist = [1, 2, "three", 4.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
 
 # re-declaring a variable works
-
+# myint = 'abc'
+# print( myint )
 # to access a member of a sequence type, use []
-
+# print( mylist[2] )
+# print( mytuple[1] )
 # use slices to get parts of a sequence
-
+# print( mylist[0:3] )
+# print( mylist[::3] )
 # you can use slices to reverse a sequence
-
+print( mylist[::-1] )
 # dictionaries are accessed via keys
-
+print( mydict['one'] )
 # ERROR: variables of different types cannot be combined
-
+print( 'my string' + str( 123 ) )
 # Global vs. local variables in functions
+def myFunction():
+    global myint
+    myint = 10
+    print( myint )
 
+myFunction()
+print( myint )
